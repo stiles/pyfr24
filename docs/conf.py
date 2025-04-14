779fe738f03b65f87b0382e9ded50f65cf225e50
Matt Stiles <mattstiles@gmail.com>
2025-04-14 10:36:00 -0700

    resolving docs build issue

---
+++ docs/conf.py
@@ -0,0 +1,16 @@
+# Minimal Sphinx configuration for Read the Docs compatibility
+project = 'Pyfr24'
+copyright = '2024, Matt Stiles'
+author = 'Matt Stiles'
+
+extensions = []
+
+# The master toctree document.
+master_doc = 'index'
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx' 
